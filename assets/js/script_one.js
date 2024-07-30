a3fddcfbbd8fcb3575b142af0b91c81edc2f746c
Ahmed Alamassi <christianalamassi@gmail.com>
2024-07-30 14:39:21 +0000

    Style the index buttom to be at the same size

--- assets/js/script_one.js
+++ assets/js/script_one.js
@@ -7,3 +7,26 @@ var squareSix = document.getElementById('six');
 var squareseven = document.getElementById('seven');
 var squareEight = document.getElementById('eight');
 var squareNine = document.getElementById('nine');
+var gameSection = document.getElementsByClassName('game_section')
+var results = document.getElementById('result')
+
+function click ({
+
+}) 
+
+/* 
+function gameOver({
+    if (computer <= player){
+            gameSection.style.display = "block";
+            result.innerHTML = "You won";
+    }
+    else if (computer == player){
+        gameSection.style.display = "block";
+        result.innerHTML = "Draw!";
+    }
+    else{
+        gameSection.style.display = "block";
+        result.innerHTML = "You lost";
+    }
+})
+*/
