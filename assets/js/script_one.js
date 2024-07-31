cef113cbc25a70aaf7aebacc21de7399ffadf84f
Ahmed Alamassi <christianalamassi@gmail.com>
2024-07-31 11:44:36 +0000

    Player can mark x

--- assets/js/script_one.js
+++ assets/js/script_one.js
@@ -1,9 +1,30 @@
 const elements = document.querySelectorAll('[data-cell]')
-var player 
-var computer
+var ticTac = document.getElementById('tic_tac')
+const restartButton = document.getElementById('restart')
+
+var player = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+] 
+var computer = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+] 
 var playeTurn
 var computerTurn
-var tic_tacs = document.getElementById('tic_tac')
+
 var gameSection = document.getElementsByClassName('game_section')
 var results = document.getElementById('result')
 
@@ -11,9 +32,13 @@ elements.forEach(pic =>{
     pic.addEventListener('click', handleClick, {once:true} )
 })
 
+// restartButton.addEventListener('click', startGame)
+
+
+
 function handleClick(e){
     var pic = e.target
-    var currentClass = computerTurn? computer:player
+    var currentClass = playeTurn? player:computer
     mark(pic, currentClass)
 }
 
@@ -48,3 +73,7 @@ function gameOver({
     }
 })
 */
+
+// function restartGame(){
+
+// }
