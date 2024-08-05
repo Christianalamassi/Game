9d6dae177a3a11c8f8ae5351f4f48592abad4d94
Ahmed Alamassi <christianalamassi@gmail.com>
2024-08-05 09:01:07 +0000

    View message after the game is over

--- assets/js/script_one.js
+++ assets/js/script_one.js
@@ -2,18 +2,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
     var elements = document.querySelectorAll('[data-cell]');
     var ticTac = document.getElementById('tic_tac');
-    var textResult = document.getElementById('result')
+    var gameSection = document.getElementById('game_section')
     var restartButton = document.getElementById('restart');
-
-    // var imgx = document.createElement('img');
-    // imgx.src = "/assets/images/x.jpg";
-    // var imgo = document.createElement('img');
-    // imgo.src = "/assets/images/o.png";
-
-
+    const results = document.getElementById('result')
     var player = 'imgx';
     var computer = 'imgo';
-
     var winningScore = [
         [6, 7, 8],
         [3, 4, 5],
@@ -24,11 +17,8 @@ window.addEventListener('DOMContentLoaded', () => {
         [2, 4, 6],
         [0, 4, 8],
     ] ;
-    running = '';
-
-    let gameState = ["", "", "", "", "", "", "", "", ""];
-
-    // restartButton.addEventListener('click', restartGame)
+    var running
+    var gameState = ["", "", "", "", "", "", "", "", ""];
 
 
     elements.forEach(cell =>{
@@ -39,14 +29,18 @@ window.addEventListener('DOMContentLoaded', () => {
         const clickedCell = cell.target;
         const currentClass = running ? computer : player
         mark(clickedCell, currentClass);
-        checkWin(currentClass)
-        // if checkWin(currentClass){
-        //     gameOver()
-        // }
-        switchTurn ()
-        computerTurn()
+        // computerTurn();
+        if (checkWin(currentClass)){
+            gameover(false)
+            console.log('win')
+        }else if (draw()) {
+            gameover(true)
+            console.log('draw')
+        } else {
+            switchTurn () 
+            console.log('switch')
+        }
     }
-
 
     function mark(clickedCell, currentClass) {
         clickedCell.classList.add(currentClass)
@@ -56,29 +50,41 @@ window.addEventListener('DOMContentLoaded', () => {
         running = !running
     }
 
-    // function result()
-    // function resartGame()
+    function gameover(nowinner){
+        if (nowinner){
+            results.innerText = "Gameover 'Draw!'"
+        }else{
+            results.innerText = `${running ? 'Computer':'You'} won`
+        }
+        gameSection.classList.add('show')
+    }
+
+    function draw(){
+        return [...elements].every(cell =>{
+            return cell.classList.contains(computer) || cell.classList.contains(player)
+        })
+    }
 
     // let the computer make the next move
     function computerTurn(){
-            const elements = gameState
-                .map((cell, index) => (cell === '' ? index : null))
-                .filter(index => index !== null);
-    
-            if (elements.length === 0) {
-                return;
-            }
-    
-            const randomIndex = elements[Math.floor(Math.random() * elements.length)];
-            board[randomIndex] = 'imgo';
-            document.querySelector(`.cell[data-index='${randomIndex}']`).innerText = 'imgo';
-            document.querySelector(`.cell[data-index='${randomIndex}']`).classList.add('taken');
-            checkResult();
-            if (gameActive) {
-                currentPlayer = 'imgx';
-                updateMessage();
-            }
+        const elements = gameState
+            .map((cell, index) => (cell === '' ? index : null))
+            .filter(index => index !== null);
+
+        if (elements.length === 0) {
+            return;
         }
+
+        const randomIndex = elements[Math.floor(Math.random() * elements.length)];
+        board[randomIndex] = 'imgo';
+        document.querySelector(`.cell[data-index='${randomIndex}']`).innerText = 'imgo';
+        document.querySelector(`.cell[data-index='${randomIndex}']`).classList.add('taken');
+        checkResult();
+        if (gameActive) {
+            currentPlayer = 'imgx';
+            updateMessage();
+        }
+    }
 
     function checkWin(currentClass) {
         return winningScore.some(combination => {
--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,7 +2,7 @@ function sendMail(contactForm) {
     emailjs.send("service_xvgrfxc", "template_6oh4tnd", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "message": contactForm.message.value
     })
     .then(
         function(response) {
